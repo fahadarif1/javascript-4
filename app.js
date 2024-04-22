acc0041ac94b52f40eb7b97e025874ca079e0896
fahadarif1 <134074010+fahadarif1@users.noreply.github.com>
2024-04-22 09:30:06 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,54 @@
+// var year = ["2022" ,"2023"];
+// var month = ["jan" , " feb" , "mar" , "april","may","june","july","august","sep","oct","nov","dec"];
+// var days =[ "monday" , "tuesday" , "wednesday", "thursday" , "friday","saturday","Sunday"]
+
+// var recordsOfEverydays= []
+
+// for (var index = 0; index < year.length; index++) {
+//     for (var innerindex = 0; innerindex < month.length; innerindex++) {
+//         for (var inner2index = 0; inner2index < days.length; inner2index++) {
+//             var combinName = year[index] + " " + month[innerindex] + " " + days[inner2index]
+//             recordsOfEverydays.push(combinName)
+//         }
+        
+//     }
+    
+    
+// }
+// console.log(recordsOfEverydays)
+// var fruit = ["apple", "mango" , "banana" , "guava" , "watermelon"]
+// var userFruit = prompt(" Enter a fruit name");
+// var isavailable = false;
+// for (let index = 0; index < fruit.length; index++) {
+    
+    
+// }
+
+
+// var arr =['ali','hassan','umer','irfan','umair','pusha'];
+// for(let i = 0; i < arr.length; i++){
+//     console.log(arr[i][0].toUpperCase());
+// }
+
+
+// var arr =['ali','hassan','umer','irfan','umair','pusha'];
+//  for(let i = 0; i < arr.length; i++){
+//      console.log(arr[i][0].toUpperCase() + arr[i].slice(1).toLowerCase());
+//     }
+
+
+// var userinput = prompt('enter your text');
+// for (var i = 0; i < userinput.length; i++){
+//     if(userinput.slice(i, i +2) === "  "){
+//         alert('no double space')
+//         break;
+//     }
+// }
+
+
+// var str = 'my name is xyz bhatti xyz'
+// var findword = 'xyz'
+
+// var index = str.indexof(findword)
+
+// if(index i =  )
